2f6bc8f43bb1fa9d8c3156ed70a8c3f59958b360
Vlad <vladdp70@gmail.com>
2021-05-21 03:31:15 +0300

    Add spred

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
-'use strict';
-const a = 3;
-const b = 4;
-const b = 4;
-const newsPaper = {
-  title: 'FC daily',
-  articles: ['Test1','Heading','String1','Article1'],
+"use strict";
+// const newsPaper = {
+//   title: 'FC daily',
+//   articles: ['Test1','Heading','String1','Article1'],
 
-  showArticles() {
-    this.articles.forEach(function logItem(currentArticle, index){
-      console.log(`${this.title} => ${index + 1}: ${currentArticle}`); 
-    });
-  },
-};
+//   showArticles() {
+//     this.articles.forEach((currentArticle, index) => {
+//       console.log(`${this.title} => ${index + 1}: ${currentArticle}`);
+//     });
+//   },
+// };
 
-newsPaper.showArticles();
+// newsPaper.showArticles();
+//////////////////////////////////
+// const square = (n) => n * n;
+
+// const t1 = function () {};
+/////////////////////////////////
+// function sum(a, b, ...rest) {
+//   console.log(rest);
+// }
+
+// const sumArrow = (...rest) => {
+//   let res = null;
+//   rest.forEach((number) => {
+//     res += number;
+//   });
+//   return res;
+// };
+//////
