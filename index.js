9f05d5b5a0823d1e392a37b8fccac21516df988e
Vlad <vladdp70@gmail.com>
2021-05-21 03:54:17 +0300

    Add factorial

--- index.js
+++ index.js
@@ -37,3 +37,11 @@ const power = (num, exp) => {
   }
   return num * power(num, exp - 1);
 };
+
+// Factorial
+function factorial(n) {
+  if (n <= 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
